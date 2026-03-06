a67dd5e269b8d12fd9117f4d508ec1ff94b047ed
Mehmet Salih KARACA <118613055+mehmetkaraca337@users.noreply.github.com>
2026-03-06 04:28:41 +0300

    css dosyasının linkine php echo filmtime ekledik güncellendiğinde her zaman son değişikliğine bakarak kendi kendine güncellemesini sağladık

--- certificates.php
+++ certificates.php
@@ -37,8 +37,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="assets/css/style.css?v=<?php echo filemtime('assets/css/style.css'); ?>">
-    <link rel="stylesheet" href="assets/css/certificates.css">
-
+    <link rel="stylesheet" href="assets/css/certificates.css?v=<?php echo filemtime('assets/css/certificates.css'); ?>">
+     
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fuse.js/7.0.0/fuse.min.js" defer></script>
 
