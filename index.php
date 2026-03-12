b701dcd69d688b638eae8ebd726686b203151068
Mehmet Salih KARACA <118613055+mehmetkaraca337@users.noreply.github.com>
2026-03-12 10:17:02 +0300

    LınkedIn script linki ekledim.

--- index.php
+++ index.php
@@ -811,6 +811,7 @@ window.addEventListener('online', () => {
     
 
     <!-- ======================== SCRIPTS ======================== -->
+     <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="assets/js/script.js?v=<?php echo filemtime('assets/js/script.js'); ?>" defer></script>
     <script src="assets/js/certificates.js" defer></script>
